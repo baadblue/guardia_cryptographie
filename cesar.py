a4243a3377bcd1905f097f1e57dd1dc01d336105
baadblue <cfremont@guardiaschool.fr>
2025-04-24 00:35:00 +0200

    Add functionalities: brute force and frequency analysis to Cesar cipher

--- cesar.py
+++ cesar.py
@@ -51,7 +51,48 @@ class CesarCipher:
     def cesar_decryption(self, chain, key):
         logger.debug("Début du déchiffrement avec la clé : %d", key)
         return self.cesar_encryption(chain, key, reverse=True)
+    
+    def brute_force_decryption(self, chain):
+        """
+        Déchiffre une chaîne de caractères en utilisant le chiffrement de César avec toutes les clés possibles.
+        
+        :param chain: chaîne à déchiffrer
+        :return: liste des chaînes déchiffrées
+        """
+        logger.debug("Début du déchiffrement par force brute.")
+        decrypted_chains = []
+        for i in range(1, 26):
+            decrypted_chains.append(self.cesar_decryption(chain, i))
+        logger.debug("Déchiffrement par force brute terminé.")
+        return decrypted_chains
+    
+    def frequency_analysis(self, chain):
+        """
+        Analyse la fréquence des lettres dans une chaîne de caractères et estime la clé de chiffrement.
+
+        :param chain: chaîne à analyser
+        :return: clé estimée
+        """
+        logger.debug("Début de l'analyse de fréquence.")
+        frequency = {}
+        for letter in chain:
+            if letter.isalpha():
+                letter = letter.upper()
+                frequency[letter] = frequency.get(letter, 0) + 1
+
+        most_common = sorted(frequency.items(), key=lambda x: x[1], reverse=True)[0]
+        logger.debug("Lettre la plus fréquente : %s avec une fréquence de %d", most_common[0], most_common[1])
+        key = ord(most_common[0]) - ord('E')
+        logger.debug("Clé estimée : %d", key)
+        
+        logger.debug("Analyse de fréquence terminée.")
+        return key
 
 if __name__ == "__main__":
-    print(CesarCipher().cesar_encryption("Hello World!", 0))
+    print(CesarCipher().cesar_encryption("Hello World!", 3))
     print(CesarCipher().cesar_decryption("KHOORZRUOG", 3))
+    print(CesarCipher().brute_force_decryption("KHOORZRUOG"))
+    text = "GHPDLQ, GHV O'DXEH, D O'KHXUH RX EODQFKLW OD FDPSDJQH, MH SDUWLUDL. YRLV-WX, MH VDLV TXH WX P'DWWHQGV.M'LUDL SDU OD IRUHW, M'LUDL SDU OD PRQWDJQH.MH QH SXLV GHPHXUHU ORLQ GH WRL SOXV ORQJWHPSV.MH PDUFKHUDL OHV BHXA ILAHV VXU PHV SHQVHHV,VDQV ULHQ YRLU DX GHKRUV, VDQV HQWHQGUH DXFXQ EUXLW,VHXO, LQFRQQX, OH GRV FRXUEH, OHV PDLQV FURLVHHV,WULVWH, HW OH MRXU SRXU PRL VHUD FRPPH OD QXLW.MH QH UHJDUGHUDL QL O'RU GX VRLU TXL WRPEH,QL OHV YRLOHV DX ORLQ GHVFHQGDQW YHUV KDUIOHXU,HW TXDQG M'DUULYHUDL, MH PHWWUDL VXU WD WRPEHXQ ERXTXHW GH KRXA YHUW HW GH EUXBHUH HQ IOHXU.GHPDLQ, GHV O'DXEH..."
+    key = CesarCipher().frequency_analysis(text)
+    print(f"Estimated key: {key}")
+    print(CesarCipher().cesar_decryption(text, key))
